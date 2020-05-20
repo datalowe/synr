0bf99f42d6e63e70469f380ff72705cda78a916b
datalowe <datalowe@posteo.de>
2020-05-20 18:53:19 +0200

    add Participant.get_prop_color() and add tests for it.
    add ParticipantGroup.get_mean_consistency_scores() and related tests.

--- tests/testthat/test-consistency_scoring.R
+++ tests/testthat/test-consistency_scoring.R
@@ -9,6 +9,7 @@ test_that("Grapheme All black color responses leads to a consistency score near 
                          1)
           })
 
+
 test_that("Grapheme All perfect blue color responses leads to a consistency score near 0",
           {
             a <- synr::Grapheme$new(symbol='a')
@@ -16,6 +17,7 @@ test_that("Grapheme All perfect blue color responses leads to a consistency scor
             expect_lt(a$get_consistency_score(),
                          1)
           })
+
 
 test_that("Grapheme 3 very different color responses leads to a consistency score above 135",
           {
@@ -25,6 +27,7 @@ test_that("Grapheme 3 very different color responses leads to a consistency scor
                          135)
           })
 
+
 test_that("Grapheme All NA color responses leads to a NA consistency score",
           {
             a <- synr::Grapheme$new(symbol='a')
@@ -32,6 +35,7 @@ test_that("Grapheme All NA color responses leads to a NA consistency score",
             is_consistency_na <- is.na(a$get_consistency_score())
             expect_true(is_consistency_na)
           })
+
 
 test_that("Grapheme Fetching consistency score before inserting responses
           produces error",
@@ -42,6 +46,7 @@ test_that("Grapheme Fetching consistency score before inserting responses
                                 ' has no registered response colors. Please register responses before calling .get_consistency_score().')
                          )
           })
+
 
 test_that("Participant get_consistency_scores() returns correct list for valid response graphemes", {
   p <- Participant$new()
@@ -59,6 +64,7 @@ test_that("Participant get_consistency_scores() returns correct list for valid r
   expect_gt(cons_list[['a']], 100)
 })
 
+
 test_that("Participant get_consistency_scores() returns correct list for nonvalid response graphemes", {
   p <- Participant$new()
   g1 <- Grapheme$new(symbol='a')
@@ -74,6 +80,7 @@ test_that("Participant get_consistency_scores() returns correct list for nonvali
   expect_true(is.na(cons_list[[2]]))
   expect_true(is.na(cons_list[['a']]))
 })
+
 
 test_that("Participant get_mean_consistency_score() returns value close to 0 for all black response graphemes", {
   p <- Participant$new()
@@ -113,3 +120,51 @@ test_that("Participant get_mean_consistency_score() returns NA value for no vali
   p$add_graphemes(g_list)
   expect_true(is.na(p$get_mean_consistency_score()))
 })
+
+
+test_that("ParticipantGroup get_mean_consistency_scores() with 1 participant
+          returns value close to 0 for all black response graphemes", {
+  p <- Participant$new(id='1')
+  g1 <- Grapheme$new(symbol='a')
+  g1$set_colors(c("#000000", "#000000", "#000000", "#000000"), "Luv")
+  g2 <- Grapheme$new(symbol='b')
+  g2$set_colors(c("#000000", "#000000", "#000000", "#000000"), "Luv")
+  g3 <- Grapheme$new(symbol='monday')
+  g3$set_colors(c("#000000", "#000000", "#000000", "#000000"), "Luv")
+  g_list <- list(g1, g2, g3)
+  p$add_graphemes(g_list)
+
+  pg <- ParticipantGroup$new()
+  pg$add_participants(list(p))
+  expect_lt(pg$get_mean_consistency_scores()[1], 0.01)
+})
+
+
+test_that("ParticipantGroup get_mean_consistency_scores() with 2 participants
+          returns value higher than 200 for very mixed response graphemes", {
+            p1 <- Participant$new(id='1')
+            g1 <- Grapheme$new(symbol='a')
+            g1$set_colors(c("#000000", "#000000", "#000000", "#000000"), "Luv")
+            g2 <- Grapheme$new(symbol='b')
+            g2$set_colors(c("#000000", "#000000", "#000000", "#000000"), "Luv")
+            g3 <- Grapheme$new(symbol='monday')
+            g3$set_colors(c("#000000", "#000000", "#000000", "#000000"), "Luv")
+            g_list1 <- list(g1, g2, g3)
+            p1$add_graphemes(g_list1)
+
+            p2 <- Participant$new(id='2')
+            g4 <- Grapheme$new(symbol='a')
+            g4$set_colors(c("#FFFFFF", "#000000", "#FFFFFF", "#000000"), "Luv")
+            g5 <- Grapheme$new(symbol='b')
+            g5$set_colors(c("#000000", "#FFFFFF", "#000000", "#FFFFFF"), "Luv")
+            g6 <- Grapheme$new(symbol='monday')
+            g6$set_colors(c("#0000FF", "#000000", "#FF0000", "#000000"), "Luv")
+            g_list2 <- list(g4, g5, g6)
+            p2$add_graphemes(g_list2)
+
+            pg <- ParticipantGroup$new()
+            pg$add_participants(list(p1, p2))
+            expect_lt(pg$get_mean_consistency_scores()[1], 0.01)
+            expect_gt(pg$get_mean_consistency_scores()[2], 200)
+          })
+
