b510fc3f8a1cd2eff922ec373a7a7986f222a031
datalowe <datalowe@posteo.de>
2020-05-23 15:19:39 +0200

    Modify create_grapheme() so that response_times are optional. Add create_participant() and a unit test for it.

--- R/create_grapheme.R
+++ R/create_grapheme.R
@@ -2,19 +2,24 @@
 #' @description Takes in a symbol/grapheme and sets of response times/colors,
 #' then creates a Grapheme instance that holds the passed information and returns it.
 #' @param symbol A one-element character vector holding a symbol/grapheme.
-#' @param response_times A numeric vector. Times from presentation to response, in order.
-#' @param response_colors A character vector. Response colors, as hex color codes.
-#' @param color_space_spec A one-element character vector. What color space is to be used? The
-#' following color spaces are supported: "XYZ", "sRGB", "Apple RGB", "Lab", and "Luv"
+#' @param response_times (optional) A numeric vector. Times from presentation to
+#' response, in order.
+#' @param response_colors A character vector. Response colors, as hex color
+#' codes.
+#' @param color_space_spec A one-element character vector. What color space
+#' is to be used? The following color spaces are supported:
+#' "XYZ", "sRGB", "Apple RGB", "Lab", and "Luv"
 #' @examples
 #' create_grapheme(symbol="a", response_times=c(2.3, 6.7, 0.4),
 #' response_colors=c("84AE99", "9E3300", "000000"), color_space_spec="Luv")
 #' @export
-create_grapheme <- function(symbol, response_times, response_colors, color_space_spec) {
+create_grapheme <- function(symbol, response_times=NULL, response_colors, color_space_spec="Luv") {
   has_no_pound <- !grepl("^#", response_colors)
   response_colors[has_no_pound] <- paste0("#", response_colors[has_no_pound])
   new_grapheme <- Grapheme$new(symbol=symbol)
-  new_grapheme$set_times(times=response_times)
+  if (!is.null(response_times)) {
+    new_grapheme$set_times(times=response_times)
+  }
   new_grapheme$set_colors(hex_codes=response_colors,
                           color_space_spec=color_space_spec)
   return(new_grapheme)
--- tests/testthat/test-raw_to_participantgroup.R
+++ tests/testthat/test-raw_to_participantgroup.R
@@ -1,7 +1,25 @@
 context("Converting raw consistency test data to a participant group with all the data bundled up")
 library(synr)
 
-# test_that("a single row of data is correctly converted", {
-#
-#   expect_equal(2 * 2, 4)
-# })
+test_that("a single row of data (split into symbol/color/time vectors)
+          is correctly converted using create_participant()", {
+  participant_id <- "1"
+  target_symbols_vec <- c("A", "D", "7")
+  symbol_vec <- c("A", "D", "7",
+                  "D", "A", "7",
+                  "7", "A", "D")
+  times_vec <- c(1.1, 0.4, 5,
+                 0.3, 2.4, 7.3,
+                 1, 10.2, 8.4)
+  color_vec <- c("98FF22", "138831", "791322",
+                 "8952FE", "DC8481", "7D89B0",
+                 "001100", "887755", "FF0033")
+  p <- create_participant(participant_id=participant_id,
+                          grapheme_symbols=target_symbols_vec,
+                          n_trials_per_grapheme=3,
+                          trial_symbols=symbol_vec,
+                          response_times=times_vec,
+                          response_colors=color_vec)
+  expect_equal(p$get_symbols()[1], "A")
+  expect_gt(p$get_mean_consistency_score(), 100)
+})
