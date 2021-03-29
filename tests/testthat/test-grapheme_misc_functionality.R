38686df45d8e050c30d9800fbe8070feea2d2964
datalowe <datalowe@posteo.de>
2021-03-29 13:59:51 +0200

    Update documentation and add get_mean_color method for Grapheme class

--- tests/testthat/test-grapheme_misc_functionality.R
+++ tests/testthat/test-grapheme_misc_functionality.R
@@ -16,3 +16,21 @@ test_that("Abbreviated symbol representation is correct", {
   g2$set_symbol('Monday')
   expect_equal(g2$get_abbreviated_symbol(), "Mon")
 })
+
+test_that("get_mean_color works correctly", {
+  a <- synr::Grapheme$new(symbol='a')
+  b <- synr::Grapheme$new(symbol='b')
+  d <- synr::Grapheme$new(symbol='d')
+  a$set_colors(c("#000000", "#000000", "#000000", "#000000"), "Luv")
+  b$set_colors(c("#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF"), "Luv")
+  d$set_colors(c("#FAFAFA", "#000000", "#FFFFFF", "#FF0000"), "Luv")
+  expect_equal(
+    a$get_mean_color(),
+    c(0, 0, 0)
+  )
+  expect_equal(
+    d$get_mean_color(),
+    c(62.94, 43.84, 9.45),
+    tolerance=0.01
+  )
+})
