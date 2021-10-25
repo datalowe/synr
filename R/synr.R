1bfa7c6db86eb0332d3617ce50e8cf8690e70dd3
datalowe <datalowe@posteo.de>
2021-10-25 16:13:17 +0200

    remove backticks as they are not correctly rendered in RStudio help doc pane

--- R/synr.R
+++ R/synr.R
@@ -1,7 +1,7 @@
 #' @description
-#' `synr` helps you work with data resulting from grapheme-color consistency tests for synesthesia.
+#' synr helps you work with data resulting from grapheme-color consistency tests for synesthesia.
 #'
-#' To learn more about `synr`, start with the vignettes:
+#' To learn more about synr, start with the vignettes:
 #' \verb{browseVignettes(package = "synr")}
 #'
 "_PACKAGE"
